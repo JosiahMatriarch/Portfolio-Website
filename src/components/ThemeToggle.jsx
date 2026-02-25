ea19b019fb2bd032662e3ff05977b1c6e39e4f7f
Corbin Brescher <cbresc2@isdsnet.bus.lsu.edu>
2026-02-25 16:57:03 -0600

    Icons not working, fix later

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -4,5 +4,14 @@ import { useState } from 'react';
 export const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
 
-    return <button> {isDarkMode ? <SunIcon /> : <MoonIcon />}</button>
+    return (
+        <button> 
+            {""}
+            {isDarkMode ? (
+                <SunIcon className='h-6 w-6 text-yellow-300'/>
+            ) : (
+                <MoonIcon className='h-6 w-6 text-blue-900'/>
+            )}
+        </button>
+    );
 }
