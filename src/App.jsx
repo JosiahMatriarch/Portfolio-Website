3c6b24c5f44f423f95723b5b34831b78b1efb9fb
JosiahMatriarch <corbinbrescher@gmail.com>
2026-02-26 00:41:54 -0600

    background effects working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { NotFound } from "./pages/NotFound"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
